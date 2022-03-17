40225f0766aa62d5a2225992789599e0a7ea2bd0
H. alrahmani <h.m.alrahmani@gmail.com>
2022-03-17 19:20:01 +0300

    FINISHED challange

--- index.js
+++ index.js
@@ -11,9 +11,11 @@
 
 // Part 1 answer 👇🏻 ...
 
-
-
-
+let fullName = "Hessa AlRahmani";
+let yearOfBirth = 1997;
+let hobby = "Roller Skating"
+let funFact = "My cat's name is Ghanima"
+const image = "https://i1.sndcdn.com/artworks-oyyH7Y75Wyacu0FU-MnZWDw-t500x500.jpg";
 
 
 /**
@@ -29,9 +31,9 @@
  * You can uncomment a line by removing the // at the start
  */
 
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
+const fullNameString = `My name is ${fullName}`;
+const yearOfBirthString = `I am ${ 2022 - yearOfBirth}`;
+const hobbyString = `My hoppy is ${hobby}`;
 
 
 
@@ -45,14 +47,18 @@ let stars = 0;
 
 function incrementBy1() {
   // Increment stars by 1 👇🏻
+  stars +=1;
 }
 function decrementBy1() {
   // decrement stars by 1 👇🏻
+  stars -=1;
 }
 
 function incrementBy2() {
   // Increment stars by 2 👇🏻
+  stars +=2;
 }
 function decrementBy2() {
   // decrement stars by 2 👇🏻
+  stars -=2;
 }
